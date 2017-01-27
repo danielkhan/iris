9d27920035ff1b5a3fde5277b10c5c0b9885a667
dkhan <datenpunk@gmail.com>
2017-01-27 12:33:58 +0100

    Moved credentials to environment variables and added config file

--- bin/run.js
+++ bin/run.js
@@ -1,14 +1,16 @@
 'use strict';
 
+const config = require('../config');
+
 const slackClient = require('../server/slackClient');
 const service = require('../server/service');
 const http = require('http');
 const server = http.createServer(service);
 
-const witToken = 'LFX6LYMSYREVOPK6VZHU6E2736US4KPI';
+const witToken = config.witToken;
 const witClient = require('../server/witClient')(witToken);
 
-const slackToken = 'xoxb-66914491142-MOYppnMN543Szy7h0JJzMEyB';
+const slackToken = config.slackToken;
 const slackLogLevel = 'verbose';
 
 const serviceRegistry = service.get('serviceRegistry');
